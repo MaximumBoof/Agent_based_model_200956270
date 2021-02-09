da1a5ac3074e4548f3c53df6672969a5321b87e0
MaximumBoof <mini_moppy@hotmail.co.uk>
2021-02-09 12:18:42 +0000

    Both agents have y and x variables replaced by list
    Both agents no longer use y and x variables, so no x0,x1, y0, y1 but instead use the indexed list reference.
    Also, I changed the random to use integers

--- amb.py
+++ amb.py
@@ -16,30 +16,30 @@ x0 = random.random() * 100
 
 # Add the first set of coordinates to the list but because of the extra square brackets, this is adding a list in a list
 # We have a two dimensional dataset, in which the first dimension agents[0] represents each agent, and the second dimension is the two coordinates, so agents[0][0] is the y coordinate and agents[0][1] is the x coordinate. As we only have one set of coordinates so far, if you print(agents) you should see something like this (though the numbers will be different):
-agents.append ([y0, x0])
+agents.append([random.randint(0,99),random.randint(0,99)])
 
 print (agents)
 
 # Random walk step one
 # For object 1 coordinates
-if random.random() < 0.5:
+if random.randint(0,99) < 50:
     agents[0][0] += 1
 else:
     agents[0][0] -= 1
     
-if random.random() < 0.5:
+if random.randint(0,99) < 50:
     agents[0][1] += 1
 else:
     agents[0][1] -= 1
 
 # Random walk step two
 # For object 1 coordinates
-if random.random() < 0.5:
+if random.randint(0,99) < 50:
     agents[0][0] += 1
 else:
     agents[0][0] -= 1
     
-if random.random() < 0.5:
+if random.randint(0,99) < 50:
     agents[0][1] += 1
 else:
     agents[0][1] -= 1
@@ -48,29 +48,31 @@ else:
 x1 = random.random() * 100
 y1 = random.random() * 100
 
+agents.append([random.randint(0,99),random.randint(0,99)])
+
 # Random walk step one
 # For object 2 coordinates
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
     
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
 # Random walk step two 
 # For object 2 coordinates
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
     
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
     
 print (y0, x0, y1, x1)
 
