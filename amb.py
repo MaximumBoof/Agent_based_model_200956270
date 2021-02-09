f012f723434530e8dde1cd331b44a016bfe1897a
MaximumBoof <mini_moppy@hotmail.co.uk>
2021-02-09 12:35:57 +0000

    Removed variablesand fixed distance

--- amb.py
+++ amb.py
@@ -10,10 +10,6 @@ import random
 #This creates an empty list
 agents = []
 
-# Set up variables (in this case coordinates) for a first object or point 
-y0 = random.random() * 100
-x0 = random.random() * 100 
-
 # Add the first set of coordinates to the list but because of the extra square brackets, this is adding a list in a list
 # We have a two dimensional dataset, in which the first dimension agents[0] represents each agent, and the second dimension is the two coordinates, so agents[0][0] is the y coordinate and agents[0][1] is the x coordinate. As we only have one set of coordinates so far, if you print(agents) you should see something like this (though the numbers will be different):
 agents.append([random.randint(0,99),random.randint(0,99)])
@@ -44,42 +40,37 @@ if random.randint(0,99) < 50:
 else:
     agents[0][1] -= 1
     
-# Set up variables (coordinates in this case) for a new object or point
-x1 = random.random() * 100
-y1 = random.random() * 100
-
+# Added another set of coordates for the second age
 agents.append([random.randint(0,99),random.randint(0,99)])
 
 # Random walk step one
 # For object 2 coordinates
-if random.random() < 0.5:
+if random.randint(0,99) < 50:
     agents[1][0] += 1
 else:
     agents[1][0] -= 1
     
-if random.random() < 0.5:
+if random.randint(0,99) < 50:
     agents[1][1] += 1
 else:
     agents[1][1] -= 1
 
 # Random walk step two 
 # For object 2 coordinates
-if random.random() < 0.5:
+if random.randint(0,99) < 50:
     agents[1][0] += 1
 else:
     agents[1][0] -= 1
     
-if random.random() < 0.5:
+if random.randint(0,99) < 50:
     agents[1][1] += 1
 else:
     agents[1][1] -= 1
     
-print (y0, x0, y1, x1)
-
 # Calculate Eudclian distance between the points (Using pythagoras)
 # Define difference in y and x
-dy = y0-y1
-dx = x0-x1
+dy = agents[0][0]-agents[1][0]
+dx = agents[0][1]-agents[1][1]
 
 # Pythogras
 distance = ((dy**2)+(dx**2))**0.5
