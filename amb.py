a43fc8b6914fe61907974ae46255ef453ea17386
MaximumBoof <mini_moppy@hotmail.co.uk>
2021-02-09 13:01:02 +0000

    Max function and import operator

--- amb.py
+++ amb.py
@@ -6,6 +6,7 @@ Created on Tue Feb  2 16:35:15 2021
 """
 
 import random
+import operator 
 
 #This creates an empty list
 agents = []
@@ -13,8 +14,6 @@ agents = []
 # Add the first set of coordinates to the list but because of the extra square brackets, this is adding a list in a list
 # We have a two dimensional dataset, in which the first dimension agents[0] represents each agent, and the second dimension is the two coordinates, so agents[0][0] is the y coordinate and agents[0][1] is the x coordinate. As we only have one set of coordinates so far, if you print(agents) you should see something like this (though the numbers will be different):
 agents.append([random.randint(0,99),random.randint(0,99)])
-
-print (agents)
 
 # Random walk step one
 # For object 1 coordinates
@@ -67,6 +66,13 @@ if random.randint(0,99) < 50:
 else:
     agents[1][1] -= 1
     
+print (agents)
+
+# This max function will give the coordinates with the largest y value
+print(max(agents))    
+# However, if we want to show the coordinates with the largest x value, we have to import operator functions
+print(max(agents, key=operator.itemgetter(1)))  
+
 # Calculate Eudclian distance between the points (Using pythagoras)
 # Define difference in y and x
 dy = agents[0][0]-agents[1][0]
