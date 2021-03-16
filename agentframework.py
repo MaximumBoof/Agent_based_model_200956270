027fb84b68f2bf411c938673d01b8a4d7f9f2748
MaximumBoof <mini_moppy@hotmail.co.uk>
2021-03-16 19:18:45 +0000

    Moving the print statements to model.py

--- agentframework.py
+++ agentframework.py
@@ -13,8 +13,6 @@ class Agent ():
     def __init__ (self):
         self.x = random.randint(0,99)
         self.y = random.randint(0,99)
-        
-        print("SP", self.x, self.y)
 
     def move(self):
         if random.random() < 0.5:
@@ -26,7 +24,4 @@ class Agent ():
             self.x = (self.x + 1) % 100
         else:
             self.x = (self.x - 1) % 100
-            
-        print("EP", self.x, self.y)
 
-    
--- model.py
+++ model.py
@@ -13,22 +13,23 @@ def distance_between(agents_row_a, agents_row_b):
     return (((agents_row_a.x - agents_row_b.x)**2) +
         ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
-num_of_agents = 1
+num_of_agents = 2
 num_of_iterations = 10
 agents = []
 
 # Make the agents.
 for i in range(num_of_agents):
      agents.append(agentframework.Agent())
-     
-    
+     print("SP", agents[i].x, agents[i].y)
 
 # Move the agents.
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
         agents[i].move()
+        print("IT", agents[i].x, agents[i].y)
 
-
+print("EP", agents[i].x, agents[i].y)
+        
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
 for i in range(num_of_agents):
