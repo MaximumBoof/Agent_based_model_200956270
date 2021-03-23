a2613ebc5511f513ac2b2c7e2c1ccf3ecd47ce07
MaximumBoof <mini_moppy@hotmail.co.uk>
2021-03-23 13:49:02 +0000

    Agents are able to share with neighbours

--- agentframework.py
+++ agentframework.py
@@ -10,10 +10,11 @@ seed = 6
 random.seed(seed)
 
 class Agent ():
-    def __init__ (self, environment):
+    def __init__ (self, environment, agents):
         self.x = random.randint(0,99)
         self.y = random.randint(0,99)
         self.environment = environment
+        self.agents = agents
         self.store = 0
 
     def move(self):
@@ -31,3 +32,19 @@ class Agent ():
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10 
+    
+    def distance_between(self, agents):
+        return (((self.x - self.x)**2) +
+            ((self.y - self.y)**2))**0.5
+    
+    def share_with_neighbours(self, neighbourhood):
+        for agent in self.agents:
+            distance = self.distance_between(agent)
+            if distance <= neighbourhood:
+                sum = self.store + agent.store
+                average = sum /2
+                self.store = average
+                agent.store = average
+                #print("sharing " + str(distance) + " " + str(average))
+       
+    
--- model.py
+++ model.py
@@ -13,8 +13,9 @@ import csv
 seed = 2
 random.seed(seed)
 
-num_of_agents = 2
+num_of_agents = 10
 num_of_iterations = 100
+neighbourhood = 20
 environment = []
 agents = []
 
@@ -26,23 +27,21 @@ for row in reader:
     for values in row:
         rowlist.append(values)
     environment.append(rowlist)
-        
-def distance_between(agents_row_a, agents_row_b):
-    return (((agents_row_a.x - agents_row_b.x)**2) +
-        ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
 # Make the agents.
 for i in range(num_of_agents):
-     agents.append(agentframework.Agent(environment))
+     agents.append(agentframework.Agent(environment, agents))
      print("SP", agents[i].x, agents[i].y)
 
 # Move the agents.
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
+        #The random is to randomise the order which the agents are processed for each iteration (not sure if I've done it right)
+        random.shuffle(agents)
         agents[i].move()
         agents[i].eat()
-        #print("IT", agents[i].x, agents[i].y)
-        
+        agents[i].share_with_neighbours(neighbourhood)
+
 for i in range(num_of_agents):
     print("EP", agents[i].x, agents[i].y)
         
@@ -53,6 +52,3 @@ for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
 matplotlib.pyplot.show()
 
-for agents_row_a in agents:
-    for agents_row_b in agents:
-        distance = distance_between(agents_row_a, agents_row_b) 
